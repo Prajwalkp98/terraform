7993972ba3a9888b0f4eb2c0e9429e147051dde0
Prajwalkp98 <prajkp98@gmail.com>
2023-03-16 15:14:28 +0530

    fadscf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
- resource "tls_private_key" "rsa" {
+resource "aws_instance" "terraform_instance1" {
+    ami = var.ami_id
+    instance_type = var.ec2_type
+    key_name = aws_key_pair.pem_file.key_name
+    tags = {
+        Name = "terraform"
+    }
+    //ghjfhj
+  
+}  
+
+resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits = 4096
 }
@@ -13,16 +24,5 @@ resource "local_file" "pem-key" {
     filename = var.key_path
   
 }
-
-resource "aws_instance" "terraform_instance1" {
-    ami = var.ami_id
-    instance_type = var.ec2_type
-    key_name = aws_key_pair.pem_file.key_name
-    tags = {
-        Name = "terraform"
-    }
-    //ghjfhj
-  
-} 
 
 
