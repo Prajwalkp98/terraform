6a007086201190240792b1ebedb0f0912e403b37
Prajwalkp98 <prajkp98@gmail.com>
2023-03-16 12:33:03 +0530

    sfv

---
+++ main-1.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "terraform_instance1" {
+    ami = "ami-0f8ca728008ff5af4"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform"
+    }
+  
+} 
+
+
+resource "aws_s3_bucket" "bucket" {
+    bucket = "prjwal9876"
+
+    tags = {
+        Name = "mybucket1"
+    }
+  
+} 
