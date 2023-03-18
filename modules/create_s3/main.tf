b21d67ea2ff7bd7b2d4cdd83f78f2cd63819e99f
Prajwalkp98 <prajkp98@gmail.com>
2023-03-18 15:37:52 +0530

    sfdsv

--- modules/create_s3/main.tf
+++ modules/create_s3/main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_backend_sse" {
 
 
 
-  resource "aws_s3_bucket_versioning" "name" {
+  resource "aws_s3_bucket_versioning" "s3_backend_versioning" {
     bucket = aws_s3_bucket.s3_backend.bucket
     versioning_configuration {
     status = "Enabled"
