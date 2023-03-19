ca76b7761312197facd7438f1ca7aa2f68faae12
Prajwalkp98 <prajkp98@gmail.com>
2023-03-19 19:33:56 +0530

    sdfvsv

--- modules/file_provisionar/main.tf
+++ modules/file_provisionar/main.tf
@@ -2,14 +2,14 @@ resource "null_resource" "file_copy" {
   connection {
     type = "ssh"
     host = var.ec2_public_ip
-    user = var.root_ec2_user
+    user = var.ec2_user
     private_key = file(var.ec2_pem)
     agent = false
   }
   
   
   provisioner "file" {
-    source      = var.root_source_path
-    destination = var.root_destination_path
+    source      = var.source_path
+    destination = var.destination_path
   }
 }
