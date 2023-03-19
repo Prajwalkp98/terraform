14e4cf2d8aa90071e8474f33a961720acb550fd5
Prajwalkp98 <prajkp98@gmail.com>
2023-03-19 18:41:49 +0530

    sdfvsv

--- variable.tf
+++ variable.tf
@@ -30,14 +30,3 @@ variable "root_dynamodb_key" {
   type = string
 }
 
-variable "root_ec2_user" {
-  type = string
-}
-
-variable "root_source_path" {
-  type = string
-}
-
-variable "root_destination_path" {
-  type = string
-}
